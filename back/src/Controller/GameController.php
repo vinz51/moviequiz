b2e835c6c1cc019a5f42d531539e6ee7ea2cab56
vinz51 <vincent1.benoit@laposte.net>
2021-04-18 22:23:08 +0200

    [back] Updates the cross origin

--- back/src/Controller/GameController.php
+++ back/src/Controller/GameController.php
@@ -15,19 +15,17 @@ class GameController
     $response = new Response(json_encode([
       'message' => 'You will play the game soon, do not be so impatient! '.$hash
     ]));
-    $response->headers->set('Content-Type', 'application/json');
 
     return $response;
   }
 
-  public function result(): Response
+  public function result(Request $request): Response
   {
     $hash = $request->attributes->get('hash');
 
     $response = new Response(json_encode([
       'message' => 'The Joker is pretty sure that you will lose the game!'
     ]));
-    $response->headers->set('Content-Type', 'application/json');
 
     return $response;
   }
