1fd3f008810597c68f6913263e14ad0519785ed0
vinz51 <vincent1.benoit@laposte.net>
2021-04-20 01:44:42 +0200

    Add a timer component

---
+++ front/src/components/timer.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import moment from "moment";
+import PropTypes from "prop-types";
+
+let intervalID = -1;
+
+const Timer = ({ onCallback, duration, delay, start }) => {
+  const [time, setTime] = useState(moment.duration(duration));
+
+  useEffect(() => {
+    if (start) {
+      intervalID = setInterval(() => {
+        if (time.asSeconds() === 0) {
+          clearInterval(intervalID);
+
+          if (onCallback) {
+            onCallback();
+          }
+        } else {
+          setTime((previousTime) =>
+            moment.duration(previousTime.asSeconds() - 1, "seconds")
+          );
+        }
+      }, delay);
+    }
+
+    return () => clearInterval(intervalID);
+  }, [time]);
+
+  return time.asSeconds();
+};
+
+Timer.propTypes = {
+  delay: PropTypes.number,
+  duration: PropTypes.object,
+  onCallback: PropTypes.func,
+  start: PropTypes.bool,
+};
+
+Timer.defaultProps = {
+  delay: 1000,
+  duration: {
+    minutes: 1,
+  },
+  onCallback: null,
+  start: false,
+};
+
+export default Timer;
